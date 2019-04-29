110d783c7950c4400098c040a3870f76cc699936
Júnior <junior.jaava@gmail.com>
2019-04-29 11:43:48 +0200

    Authorizing access to some urls and blocking access to another urls

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,9 +1,26 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvcSecurity
-public class SecurityConfiguration extends WebMvcConfigurerAdapter{
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	    .antMatchers("/produtos/form").hasRole("ADMIN")
+	    .antMatchers("/resources/**").permitAll()
+	    .antMatchers("/carrinho/**").permitAll()
+	    .antMatchers("/pagamento/**").permitAll()
+		    .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+		    .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+	    .antMatchers("/produtos/**").permitAll()
+	    .antMatchers("/").permitAll()
+	    .anyRequest().authenticated()
+	    .and().formLogin();
+	}
 
 }
